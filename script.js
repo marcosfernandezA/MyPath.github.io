b5f2348d209b3d2a5c133a9a2d0c0a2d00246d18
marcosfernandezA <marcos.feralonso@gmail.com>
2025-10-06 17:34:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-function showSection(id) {
-    document.querySelectorAll('.content > div').forEach(div => {
-      div.style.display = 'none';
+// Navegación entre secciones
+function showSection(sectionId) {
+    document.querySelectorAll('main section').forEach(sec => {
+        sec.classList.add('hidden');
     });
-    document.getElementById(id).style.display = 'block';
-  }
+    document.getElementById(sectionId).classList.remove('hidden');
+}
+
+// Enlaces de la barra de navegación
+window.onload = function() {
+    // Mostrar índice al cargar
+    showSection('index');
+
+    document.getElementById('web-security-link').onclick = function(e) {
+        e.preventDefault();
+        showSection('web-security-index');
+    };
+    document.getElementById('xss-link').onclick = function(e) {
+        e.preventDefault();
+        showSection('xss');
+    };
+    document.getElementById('sql-injection-link').onclick = function(e) {
+        e.preventDefault();
+        showSection('sql-injection');
+    };
+    document.querySelector('a[href="#devsecops"]').onclick = function(e) {
+        e.preventDefault();
+        showSection('devsecops');
+    };
+    document.querySelector('a[href="#design-security"]').onclick = function(e) {
+        e.preventDefault();
+        showSection('design-security');
+    };
+};
